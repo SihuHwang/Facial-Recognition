d6605b8cf152d1f667df83bc37943868464e36da
SihuHwang <sihu.hwang@capboise.org>
2023-12-20 22:34:59 -0700

    added new program and folder for the ai model

--- main.py
+++ main.py
@@ -1,16 +1,56 @@
-import cv2
-import time
-vid = cv2.VideoCapture(0) 
-img1 = cv2.imread('faces/elonmusk.jpg')
-img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-ret, frame = vid.read() 
-if ret:
-    cv2.imshow("GeeksForGeeks", frame)
-    cv2.imwrite("GeeksForGeeks.png", frame) 
+# import the opencv library 
+import cv2 
+import matplotlib.pyplot as plt
 
-    # If keyboard interrupt occurs, destroy image  
-    # window 
-    cv2.waitKey(0) 
-    cv2.destroyWindow("GeeksForGeeks") 
-    time.sleep
+#grabs the image path
+imagePath = 'person.jpg'
 
+img = cv2.imread(imagePath)
+
+#Changes picture to greyscale   
+grey_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#Loads the face classifer class
+face_classifier = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+)
+
+face = face_classifier.detectMultiScale(
+    grey_img, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40)
+)
+for (x, y, w, h) in face:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 4)
+
+img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+# cv2.imshow('img', img_rgb)
+# cv2.waitKey(0)
+
+#starts the video feed
+video_capture = cv2.VideoCapture(0)
+def detect_bounding_box(vid):
+    gray_image = cv2.cvtColor(vid, cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray_image, 1.1, 5, minSize=(40, 40))
+    for (x, y, w, h) in faces:
+        cv2.rectangle(vid, (x, y), (x + w, y + h), (0, 255, 0), 4)
+    return faces
+
+while True:
+
+    result, video_frame = video_capture.read()  # read frames from the video
+    if result is False:
+        break  # terminate the loop if the frame is not read successfully
+
+    faces = detect_bounding_box(
+        video_frame
+    )  # apply the function we created to the video frame
+
+    cv2.imshow(
+        "My Face Detection Project", video_frame
+    )  # display the processed frame in a window named "My Face Detection Project"
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
